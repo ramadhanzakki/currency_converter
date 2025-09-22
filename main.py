e21b92714ea8a6f83cb1725604637322c34bdeef
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2025-09-22 22:32:55 +0700

    refactor(converter): Implement dictionary-based exchange rates
    The previous currency conversion logic using if/elif statements was not scalable. Adding a new currency required adding multiple new conditions, making the code complex and hard to maintain.
    
    This commit introduces a more robust and scalable solution by:
    - Using a dictionary to store exchange rates relative to a base currency (USD).
    - Simplifying the conversion logic into a single, reusable formula.
    - Adding initial support for EUR and CAD to match the project requirements.

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
+EXCHANGE_RATES = {
+    'IDR': 16500.0,
+    'EUR': 0.85,
+    'USD': 1.0,
+    'JPY': 147.87
+}
+
+
 def currency_converter(amount, source, target):
-    if target == 'idr' and source == 'usd':
-        return amount * 16500
-    elif target == 'usd' and source == 'idr':
-        return amount / 16500
-    else:
-        return amount
+    if source_currency not in EXCHANGE_RATES or target_currency not in EXCHANGE_RATES:
+        return None
+    convert_to_usd = amount / EXCHANGE_RATES[source]
+    convert_usd_to_target = convert_to_usd * EXCHANGE_RATES[target]
+    return convert_usd_to_target
 
 
 while True:
+    try:
+        amount = float(input('Enter the amount: '))
+        break
+    except:
+        print('Input just number!')
+        continue
 
-    while True:
-        try:
-            amount = int(input('Enter the amount: '))
-            break
-        except:
-            print('Input just number!')
-            continue
+while True:
+    source_currency = input(f'Source currency ({"/".join(EXCHANGE_RATES.keys())}): ').upper()
+    if source_currency in EXCHANGE_RATES:
+        break
+    else:
+        print('Invalid Input! Please chocie from the available currency')
 
-    while True:
-        source_currency = input('Source currency (USD/IDR): ').lower()
-        if source_currency == 'usd' or source_currency == 'idr':
-            break
-        else:
-            print('Choice between USD or IDR')
-            continue
-
-    while True:
-        target_currency = input('Target currency (USD/IDR): ').lower()
-        if target_currency == 'usd' or target_currency == 'idr':
-            break
-        else:
-            print('Choice between USD or IDR')
-            continue
-    
-    break
+while True:
+    target_currency = input(f'Target currency ({"/".join(EXCHANGE_RATES.keys())}): ').upper()
+    if source_currency in EXCHANGE_RATES:
+        break
+    else:
+        print('Invalid Input! Please chocie from the available currency')
 
 results_conveter = currency_converter(amount, source_currency, target_currency)
 
-print(f'{amount} {source_currency.capitalize()} is equal to {results_conveter} {target_currency.capitalize()}')
+print(f'{amount} {source_currency} is equal to {results_conveter} {target_currency}')
